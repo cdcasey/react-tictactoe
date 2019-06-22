fb70ff39e90ab561efada37910ea4282dc6f4c66
Chris Casey <cdcasey@gmail.com>
2019-06-22 14:08:59 -0500

    Package updates

--- src/test.js
+++ src/test.js
@@ -4,13 +4,13 @@
 const theArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const matrixWidth = 7;
 for (let i = 0; i < theArr.length; i++) {
-    const element = theArr[i];
-    const col = i % matrixWidth;
-    const row = Math.floor(i / matrixWidth);
-    console.log(col, row);
-    console.log(getIndexByCoordinate(col, row));
+  const element = theArr[i];
+  const col = i % matrixWidth;
+  const row = Math.floor(i / matrixWidth);
+  console.log(col, row);
+  console.log(getIndexByCoordinate(col, row));
 }
 
 function getIndexByCoordinate(col, row) {
-    return row * matrixWidth + col;
+  return row * matrixWidth + col;
 }
