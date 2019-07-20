d03e26c8c62bd5bb053142f742145ac664215092
Chris Casey <cdcasey@gmail.com>
2019-07-20 17:22:31 -0500

    Adding game provider

---
+++ src/providers/GameProvider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const GameContext = React.useContext();
+
+export function GameProvider(props) {
+  const [history, setHistory] = React.useState([{
+    squares: Array(9).fill(null),
+  }]);
+  const [stepNumber, setStepNumber] = React.useState(0);
+  const [xIsNext, setXIsNext] = React.useState(true);
+  const [ascending, setAscending] = React.useState(false);
+
+  const value = {
+    history, setHistory, stepNumber, setStepNumber, xIsNext, setXIsNext, ascending, setAscending,
+  };
+
+  return <GameContext.Provider value={value} {...props} />;
+}
