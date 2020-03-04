5e1c353aa6acef7c766ec790640266cc1f4fb884
Chris Casey <cdcasey@gmail.com>
2020-03-03 18:10:33 -0600

    Don't call API on rerender

--- src/components/SWComponent.jsx
+++ src/components/SWComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function SWComponent({ name, getSW, stepNumber }) {
-  getSW(stepNumber);
+  React.useEffect(() => {
+    getSW(stepNumber);
+  }, [stepNumber, getSW]);
   return <div>{name}</div>;
 }
