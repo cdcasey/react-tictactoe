94b3c04c07afc24f9ef0a97783585d22ea25ba52
Chris Casey <cdcasey@users.noreply.github.com>
2022-03-25 21:08:22 -0500

    leaned out component

--- src/components/SWComponent.jsx
+++ src/components/SWComponent.jsx
@@ -1,22 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useGetCharacterQuery } from 'redux/sw/swSlice';
 
-// eslint-disable-next-line object-curly-newline
-export default function SWComponent({ name, getSW, stepNumber }) {
-  // React.useEffect(() => {
-  //   getSW(stepNumber);
-  // }, [stepNumber, getSW]);
-
+export default function SWComponent() {
+  const stepNumber = useSelector((state) => state.game.stepNumber);
   const { data, error, isLoading } = useGetCharacterQuery(stepNumber);
-  console.log(data);
+
   if (isLoading) {
     return <div>Feel the Force...</div>;
   }
 
   if (error) {
-    console.log(error);
-    return <div>{JSON.stringify(error)}</div>;
+    return <div>{JSON.stringify(error, null, 2)}</div>;
   }
 
   return <div>{data.name}</div>;
